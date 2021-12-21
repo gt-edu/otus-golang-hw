a816cad8f1ff76b796f4d9a8a3e72affe435c533
gt-edu <95433054+gt-edu@users.noreply.github.com>
2021-12-21 09:50:02 +0300

    HW02 Unpack String (#2)
    * HW02 init implementation
    
    * HW02 : go mod tidy
    
    * Fix 'File is not `gofumpt`-ed'; function extraction
    
    * Добавлена поддержка многобайтовых символов
    
    * Исправлена ошибка игнорирования любых других символов кроме букв и цифр

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,60 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(inp string) (string, error) {
+	outBuilder := strings.Builder{}
+	var lastSymbol rune
+	runes := []rune(inp)
+
+	for pos := 0; pos < len(runes); pos++ {
+		c := runes[pos]
+
+		switch {
+		case unicode.IsDigit(c):
+			if lastSymbol == 0 {
+				return "", ErrInvalidString
+			}
+			repCount, err := strconv.Atoi(string(c))
+			if err != nil {
+				return "", err
+			}
+
+			if repCount > 0 {
+				outBuilder.WriteString(strings.Repeat(string(lastSymbol), repCount))
+			}
+
+			lastSymbol = 0
+		case string(c) == `\`:
+			handleLastSymbol(lastSymbol, &outBuilder)
+
+			pos++
+			if pos >= len(runes) {
+				return "", ErrInvalidString
+			}
+			lastSymbol = runes[pos]
+			if string(lastSymbol) != `\` && !unicode.IsDigit(lastSymbol) {
+				return "", ErrInvalidString
+			}
+		default:
+			handleLastSymbol(lastSymbol, &outBuilder)
+			lastSymbol = c
+		}
+	}
+
+	handleLastSymbol(lastSymbol, &outBuilder)
+
+	return outBuilder.String(), nil
+}
+
+func handleLastSymbol(lastSymbol rune, outBuilder *strings.Builder) {
+	if lastSymbol != 0 {
+		outBuilder.WriteRune(lastSymbol)
+	}
 }
