1253a3cf08103c75b3e0eb4d020b9547a7dfb317
Georgy Turevich <gt.edu.acc@gmail.com>
2021-12-27 10:23:16 +0300

    Реализация с помощью SortSlice функционала

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -6,44 +6,70 @@ import (
 	"strings"
 )
 
+type FrequencyStringSlice struct {
+	sort.StringSlice
+	uniqWordsCount map[string]int
+}
+
+func (x FrequencyStringSlice) Less(i, j int) bool {
+	if x.uniqWordsCount[x.StringSlice[i]] == x.uniqWordsCount[x.StringSlice[j]] {
+		return x.StringSlice[i] < x.StringSlice[j]
+	}
+	return x.uniqWordsCount[x.StringSlice[i]] > x.uniqWordsCount[x.StringSlice[j]]
+}
+
+func (x *FrequencyStringSlice) StringExists(word string) bool {
+	_, ok := x.uniqWordsCount[word]
+	return ok
+}
+
+func (x *FrequencyStringSlice) AddWord(word string) {
+	x.uniqWordsCount[word] = 0
+	x.StringSlice = append(x.StringSlice, word)
+}
+
+func (x *FrequencyStringSlice) IncreaseWordCount(word string) {
+	x.uniqWordsCount[word]++
+}
+
+func NewFrequencyStringSlice() *FrequencyStringSlice {
+	return &FrequencyStringSlice{
+		StringSlice:    make([]string, 0),
+		uniqWordsCount: make(map[string]int),
+	}
+}
+
 func Top10(inp string) []string {
 	if len(inp) == 0 {
 		return nil
 	}
 
+	freqStrSl := NewFrequencyStringSlice()
 	words := strings.Fields(inp)
-	uniqWordsCount := make(map[string]int)
-	var uniqWords []string
 	for _, w := range words {
-		_, ok := uniqWordsCount[w]
+		ok := freqStrSl.StringExists(w)
 		if !ok {
 			if len(w) == 0 {
 				continue
 			}
-			uniqWordsCount[w] = 0
-			uniqWords = append(uniqWords, w)
+			freqStrSl.AddWord(w)
 		}
 
-		uniqWordsCount[w]++
+		freqStrSl.IncreaseWordCount(w)
 	}
 
-	sort.Slice(uniqWords, func(i, j int) bool {
-		if uniqWordsCount[uniqWords[i]] == uniqWordsCount[uniqWords[j]] {
-			return uniqWords[i] < uniqWords[j]
-		}
-		return uniqWordsCount[uniqWords[i]] > uniqWordsCount[uniqWords[j]]
-	})
+	sort.Sort(freqStrSl)
 
-	if len(uniqWords) == 0 {
+	if freqStrSl.Len() == 0 {
 		return nil
 	}
 
 	min := 10
-	if len(uniqWords) < 10 {
-		min = len(uniqWords)
+	if freqStrSl.Len() < 10 {
+		min = freqStrSl.Len()
 	}
 
-	return uniqWords[:min]
+	return freqStrSl.StringSlice[:min]
 }
 
 func PrintSliceValues(checkSlice []string, countMap map[string]int) {
