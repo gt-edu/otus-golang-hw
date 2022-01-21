ec8616956194fab280cab85b9060ece4094b0b44
Georgy Turevich <gt.edu.acc@gmail.com>
2022-01-21 10:24:15 +0300

    Первая реализация списка

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,5 +1,10 @@
 package hw04lrucache
 
+import (
+	"fmt"
+	"strings"
+)
+
 type List interface {
 	Len() int
 	Front() *ListItem
@@ -17,8 +22,116 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	front *ListItem
+	back  *ListItem
+	len   int
+}
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+func (l *list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newFront := &ListItem{
+		Value: v,
+	}
+
+	l.pushItemToFront(newFront)
+
+	return newFront
+}
+
+func (l *list) pushItemToFront(newFront *ListItem) {
+	if l.front != nil {
+		l.front.Prev = newFront
+		newFront.Next = l.front
+	} else if l.back != nil {
+		l.back.Prev = newFront
+		newFront.Next = l.back
+	}
+
+	if l.len == 1 && l.back == nil {
+		l.back = l.front
+	}
+
+	newFront.Prev = nil
+
+	l.front = newFront
+
+	l.len++
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newBack := &ListItem{
+		Value: v,
+	}
+
+	if l.back != nil {
+		l.back.Next = newBack
+		newBack.Prev = l.back
+	} else if l.front != nil {
+		l.front.Next = newBack
+		newBack.Prev = l.front
+	}
+
+	if l.len == 1 && l.front == nil {
+		l.front = l.back
+	}
+
+	l.back = newBack
+
+	l.len++
+
+	return newBack
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i.Prev == nil {
+		// it is front
+		l.front = i.Next
+	} else {
+		i.Prev.Next = i.Next
+	}
+
+	if i.Next == nil {
+		// it is back
+		l.back = i.Prev
+	} else {
+		i.Next.Prev = i.Prev
+	}
+
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	l.Remove(i)
+	l.pushItemToFront(i)
+}
+
+func (l *list) String() string {
+	sb := strings.Builder{}
+	for curr := l.front; curr != nil; curr = curr.Next {
+		sb.WriteString(fmt.Sprintf("%v ", curr.Value))
+	}
+
+	return sb.String()
+}
+
+func ListToIntArray(l List) []int {
+	arr := make([]int, 0, l.Len())
+	for curr := l.Front(); curr != nil; curr = curr.Next {
+		arr = append(arr, curr.Value.(int))
+	}
+
+	return arr
 }
 
 func NewList() List {
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -49,3 +49,149 @@ func TestList(t *testing.T) {
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
 	})
 }
+
+func TestListFuncs(t *testing.T) {
+	t.Run("PushFront", func(t *testing.T) {
+		l := NewList()
+		l.PushFront(1)
+		require.Equal(t, 1, l.Front().Value)
+		require.Nil(t, l.Front().Prev)
+		require.Nil(t, l.Front().Next)
+		require.Nil(t, l.Back())
+
+		l.PushFront(2)
+		require.Equal(t, 2, l.Front().Value)
+		require.Nil(t, l.Front().Prev)
+		require.NotNil(t, l.Front().Next)
+		require.NotNil(t, l.Back())
+		require.Equal(t, l.Front().Next, l.Back())
+
+		l = NewList()
+		l.PushBack(3)
+		l.PushFront(2)
+
+		require.Equal(t, 2, l.Front().Value)
+		require.Nil(t, l.Front().Prev)
+		require.NotNil(t, l.Front().Next)
+		require.NotNil(t, l.Back())
+		require.Equal(t, l.Front().Next, l.Back())
+		require.Equal(t, 3, l.Back().Value)
+
+		l.PushFront(1)
+		require.Equal(t, []int{1, 2, 3}, ListToIntArray(l))
+	})
+	t.Run("PushBack", func(t *testing.T) {
+		l := NewList()
+		l.PushBack(1)
+		require.Equal(t, 1, l.Back().Value)
+		require.Equal(t, 1, l.Len())
+		require.Nil(t, l.Back().Prev)
+		require.Nil(t, l.Back().Next)
+		require.Nil(t, l.Front())
+
+		l.PushBack(2)
+		require.Equal(t, 2, l.Back().Value)
+		require.Equal(t, 2, l.Len())
+		require.NotNil(t, l.Back().Prev)
+		require.Nil(t, l.Back().Next)
+		require.NotNil(t, l.Front())
+
+		l.PushBack(3)
+		require.Equal(t, 3, l.Back().Value)
+		require.Equal(t, 3, l.Len())
+		require.NotNil(t, l.Back().Prev)
+		require.Nil(t, l.Back().Next)
+		require.NotNil(t, l.Front())
+
+		l = NewList()
+		l.PushFront(1)
+		l.PushBack(2)
+		require.Equal(t, 2, l.Back().Value)
+		require.Equal(t, 2, l.Len())
+		require.NotNil(t, l.Back().Prev)
+		require.Nil(t, l.Back().Next)
+		require.NotNil(t, l.Front())
+		require.Equal(t, 1, l.Front().Value)
+
+		l.PushBack(3)
+		require.Equal(t, []int{1, 2, 3}, ListToIntArray(l))
+	})
+
+	t.Run("Remove", func(t *testing.T) {
+		l := NewList()
+		l.PushFront(2)
+		l.PushBack(3)
+		l.PushFront(1)
+		require.Equal(t, []int{1, 2, 3}, ListToIntArray(l))
+		require.Equal(t, 3, l.Len())
+
+		l.Remove(l.Back())
+		require.Equal(t, []int{1, 2}, ListToIntArray(l))
+		require.Equal(t, 2, l.Len())
+
+		l.Remove(l.Front())
+		require.Equal(t, []int{2}, ListToIntArray(l))
+		require.Equal(t, 1, l.Len())
+
+		l.Remove(l.Front())
+		require.Equal(t, []int{}, ListToIntArray(l))
+		require.Equal(t, 0, l.Len())
+
+		l = NewList()
+		l.PushFront(2)
+		l.PushBack(3)
+		l.PushFront(1)
+		require.Equal(t, []int{1, 2, 3}, ListToIntArray(l))
+
+		l.Remove(l.Front())
+		require.Equal(t, []int{2, 3}, ListToIntArray(l))
+
+		l = NewList()
+		l.PushBack(2)
+		l.PushFront(1)
+		l.PushBack(3)
+		require.Equal(t, []int{1, 2, 3}, ListToIntArray(l))
+
+		l.Remove(l.Front())
+		require.Equal(t, []int{2, 3}, ListToIntArray(l))
+	})
+
+	t.Run("MoveToFront", func(t *testing.T) {
+		l := NewList()
+		l.PushFront(2)
+		l.PushBack(3)
+		l.PushFront(1)
+		require.Equal(t, []int{1, 2, 3}, ListToIntArray(l))
+		require.Equal(t, 3, l.Back().Value)
+
+		l.MoveToFront(l.Front())
+		require.Equal(t, []int{1, 2, 3}, ListToIntArray(l))
+
+		l.MoveToFront(l.Back())
+		require.Equal(t, []int{3, 1, 2}, ListToIntArray(l))
+		require.Equal(t, 2, l.Back().Value)
+
+		l.MoveToFront(l.Front().Next)
+		require.Equal(t, []int{1, 3, 2}, ListToIntArray(l))
+
+		l = NewList()
+		l.PushFront(2)
+		l.PushBack(3)
+		l.PushFront(1)
+		require.Equal(t, []int{1, 2, 3}, ListToIntArray(l))
+
+		l.MoveToFront(l.Front())
+		require.Equal(t, []int{1, 2, 3}, ListToIntArray(l))
+
+		l.MoveToFront(l.Back().Prev)
+		require.Equal(t, []int{2, 1, 3}, ListToIntArray(l))
+	})
+
+	t.Run("String", func(t *testing.T) {
+		l := NewList()
+		l.PushBack(1)
+		l.PushBack(2)
+		l.PushBack(3)
+		require.Equal(t, "1 2 3 ", l.(*list).String())
+	})
+}
