4449d04d034457f8e8c1f66b84b130bc063cbb64
Georgy Turevich <gt.edu.acc@gmail.com>
2022-08-31 11:25:25 +0300

    Initial implementation (before golangci-lint)

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,84 @@ package main
 
 import (
 	"errors"
+	"github.com/cheggaaa/pb/v3"
+	"io"
+	"os"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+
+	ErrOffsetAndLimitShouldBePositive = errors.New("offset and limit should be positive")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fromFile, fromFileSize, err := validateParams(fromPath, offset, limit)
+	if err != nil {
+		return err
+	}
+
+	if offset > 0 {
+		_, err := fromFile.Seek(offset, io.SeekStart)
+		if err != nil {
+			return err
+		}
+	}
+
+	copyUntilEnd := limit == 0 || (offset+limit) > fromFileSize
+
+	bar, barReader := setupProgressBar(fromFile, fromFileSize, copyUntilEnd, offset, limit)
+
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	if copyUntilEnd {
+		_, err = io.Copy(toFile, barReader)
+	} else {
+		_, err = io.CopyN(toFile, barReader, limit)
+	}
+
+	if err != nil && err != io.EOF {
+		return err
+	}
+
+	bar.Finish()
 	return nil
+}
+
+func setupProgressBar(fromFile *os.File, fromFileSize int64, copyUntilEnd bool, offset int64, limit int64) (*pb.ProgressBar, *pb.Reader) {
+	pbCount := fromFileSize
+	if copyUntilEnd {
+		pbCount = pbCount - offset
+	} else {
+		pbCount = limit
+	}
+	bar := pb.Full.Start64(pbCount)
+	barReader := bar.NewProxyReader(fromFile)
+	return bar, barReader
+}
+
+func validateParams(fromPath string, offset int64, limit int64) (*os.File, int64, error) {
+	if offset < 0 || limit < 0 {
+		return nil, 0, ErrOffsetAndLimitShouldBePositive
+	}
+
+	fromFile, err := os.Open(fromPath)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	fromFileInfo, err := fromFile.Stat()
+	if err != nil {
+		return nil, 0, err
+	}
+	fromFileSize := fromFileInfo.Size()
+	if offset > fromFileSize {
+		return nil, 0, ErrOffsetExceedsFileSize
+	}
+
+	return fromFile, fromFileSize, nil
 }
--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -1,3 +1,8 @@
-module github.com/fixme_my_friend/hw07_file_copying
+module github.com/gt-edu/otus-golang-hw/hw07_file_copying
 
 go 1.16
+
+require (
+	github.com/cheggaaa/pb/v3 v3.1.0 // indirect
+	github.com/stretchr/testify v1.8.0
+)
