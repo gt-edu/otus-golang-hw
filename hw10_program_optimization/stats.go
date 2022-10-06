7615018b7adc4e8ad0bb92005ef032a77a1079d6
Georgy Turevich <gt.edu.acc@gmail.com>
2022-10-06 13:26:06 +0300

    Use Readline instead of Scanner; make User smaller
    ```
    $ go test -v -count=1 -timeout=30s -tags bench .
    === RUN   TestGetDomainStat_Time_And_Memory
        stats_optimization_test.go:49: time used: 105.315347ms / 300ms
        stats_optimization_test.go:50: memory used: 3Mb / 30Mb
    --- PASS: TestGetDomainStat_Time_And_Memory (1.06s)
    PASS
    ok  	github.com/gt-edu/otus-golang-hw/hw10_program_optimization	1.064s
    
    $ go test -tags bench -v -bench=BenchmarkGetDomainStat -run=^$ -benchmem -benchtime 30s > 30s_readline.out
    
    benchstat  -delta-test none 30s.out 30s_readline.out
    name             old time/op    new time/op    delta
    GetDomainStat-4    1.80ms ± 0%    0.00ms ± 0%  -99.96%
    
    name             old alloc/op   new alloc/op   delta
    GetDomainStat-4    7.54kB ± 0%    4.14kB ± 0%  -45.00%
    
    name             old allocs/op  new allocs/op  delta
    GetDomainStat-4      91.0 ± 0%       2.0 ± 0%  -97.80%
    ```

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,55 +1,33 @@
 package hw10programoptimization
 
 import (
-	"fmt"
+	"bufio"
 	"io"
-	"io/ioutil"
 	"strings"
 )
 
 type User struct {
-	ID       int
-	Name     string
-	Username string
-	Email    string
-	Phone    string
-	Password string
-	Address  string
+	Email string
 }
 
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
-}
-
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := ioutil.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = user.UnmarshalJSON([]byte(line)); err != nil {
-			return
-		}
-		result[i] = user
-	}
-	return
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
 	result := make(DomainStat)
 
-	for _, user := range u {
+	reader := bufio.NewReader(r)
+
+	for {
+		line, _, err := reader.ReadLine()
+
+		if err == io.EOF {
+			break
+		}
+
+		var user User
+		if err := user.UnmarshalJSON([]byte(line)); err != nil {
+			return nil, err
+		}
 
 		if strings.HasSuffix(user.Email, domain) {
 			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]++
