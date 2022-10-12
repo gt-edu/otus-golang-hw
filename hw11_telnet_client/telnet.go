88f057bba2c6d6a7d95a8b11cfb8f5f3c543c505
Georgy Turevich <gt.edu.acc@gmail.com>
2022-10-12 13:27:31 +0300

    First TelnetClient implementation

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"net"
 	"time"
 )
 
@@ -14,8 +15,70 @@ type TelnetClient interface {
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
 	// Place your code here.
+	return &TelnetClientImpl{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+type TelnetClientImpl struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+
+	// Нужно ли здесь указатель
+	conn net.Conn
+}
+
+func (t *TelnetClientImpl) Connect() error {
+	var err error
+	t.conn, err = net.Dial("tcp", t.address)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *TelnetClientImpl) Close() error {
+	if t.conn != nil {
+		err := t.conn.Close()
+		if err != nil {
+			return err
+		}
+		t.conn = nil
+	}
+
+	return nil
+}
+
+func (t *TelnetClientImpl) Send() error {
+	p := make([]byte, 10000)
+	n, err := t.in.Read(p)
+	if err != nil {
+		return err
+	}
+	_, err = t.conn.Write(p[:n])
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *TelnetClientImpl) Receive() error {
+	p := make([]byte, 10000)
+	n, err := t.conn.Read(p)
+	if err != nil {
+		return err
+	}
+
+	_, err = t.out.Write(p[:n])
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
